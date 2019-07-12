d1ca1068eadf4ad0e56a435ea570367d6be30710
Vince Lozano <vincelozano@Vinces-MacBook-Air.local>
2019-07-12 15:26:01 -0700

    added listener to server for PORT 8080.

--- server.js
+++ server.js
@@ -3,7 +3,21 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 
-var Port = process.env.port || 8080;
+var PORT = process.env.port || 8080;
 
 // creates the application/Json parser
 var jsonParser = bodyParser.json()
+
+var urlencodedParser = bodyParser.urlencoded({ extended : false})
+
+// parse different custom json types as json
+app.use(bodyParser.json({ type: "application/**json" }))
+// parse some custom data into a buffer
+app.use(bodyParser.raw({ type: "application/vnd.custom-type" }))
+// parse an html body into a string
+app.use(bodyParser.text({ type: "text/html" }))
+
+//listener for server
+app.listen(PORT, function() {
+    console.log("APP Listening on Port: " + PORT);
+});
